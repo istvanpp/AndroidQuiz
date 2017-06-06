c9546d6e205505892bcee9c95fe33ff7488a7c53
istvanpp <bigpityuka@yahoo.com>
2017-06-06 18:17:13 +0300

    Version 2: XML code corrections, use of Butter Knife library

--- app/src/main/java/ro/palkoistvan/android/androidquiz/QuizActivity.java
+++ app/src/main/java/ro/palkoistvan/android/androidquiz/QuizActivity.java
@@ -4,17 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class QuizActivity extends AppCompatActivity {
+
+    // Bind Views with Butter Knife
+    @BindView(R.id.q2answer1)
+    CheckBox question2option1;
+    @BindView(R.id.q2answer2)
+    CheckBox question2option2;
+    @BindView(R.id.q2answer3)
+    CheckBox question2option3;
+    @BindView(R.id.q1_radiobuttongroup)
+    RadioGroup question1RadioGroup;
+    @BindView(R.id.q3answer1)
+    EditText question3Text;
+    @BindView(R.id.q4_radiobuttongroup)
+    RadioGroup question4RadioGroup;
+    @BindView(R.id.q5answer1)
+    CheckBox question5option1;
+    @BindView(R.id.q5answer2)
+    CheckBox question5option2;
+    @BindView(R.id.q5answer3)
+    CheckBox question5option3;
+    @BindView(R.id.q5answer4)
+    CheckBox question5option4;
+    @BindView(R.id.q6_radiobuttongroup)
+    RadioGroup question6RadioGroup;
+    @BindView(R.id.submitQuiz)
+    Button submitQuizBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // ButterKnife is instructed to bind the annotated views
+        ButterKnife.bind(this);
     }
 
     /**
@@ -27,58 +60,43 @@ public class QuizActivity extends AppCompatActivity {
         boolean setAllOptions = true;
 
         // Process question no. 1
-        RadioGroup question1RadioGroup = (RadioGroup) findViewById(R.id.q1_radiobuttongroup);
         if (question1RadioGroup.getCheckedRadioButtonId() == R.id.q1answer4) {
             points = points + 1;
         }
 
         // Process question no. 2
         setAllOptions = true;
-        CheckBox question2option1 = (CheckBox) findViewById(R.id.q2answer1);
         if (!question2option1.isChecked()) {
             setAllOptions = false;
-        }
-        CheckBox question2option2 = (CheckBox) findViewById(R.id.q2answer2);
-        if (question2option2.isChecked()) {
+        } else if (question2option2.isChecked()) {
             setAllOptions = false;
-        }
-        CheckBox question2option3 = (CheckBox) findViewById(R.id.q2answer3);
-        if (!question2option3.isChecked()) {
+        } else if (!question2option3.isChecked()) {
             setAllOptions = false;
-        }
-        if (setAllOptions) {
+        } else if (setAllOptions) {
             points = points + 1;
         }
 
-        // Evaluate question 3
-        EditText question3Text = (EditText) findViewById(R.id.q3answer1);
+        // Process question 3
         String question3Answer = question3Text.getText().toString().trim().toLowerCase();
         if (question3Answer.equals("java")) {
             points = points + 1;
         }
 
         // Process question no. 4
-        RadioGroup question4RadioGroup = (RadioGroup) findViewById(R.id.q4_radiobuttongroup);
         if (question4RadioGroup.getCheckedRadioButtonId() == R.id.q4answer2) {
             points = points + 1;
         }
 
         // Process question no. 5
-        setAllOptions = true;
-        CheckBox question6option1 = (CheckBox) findViewById(R.id.q5answer1);
-        if (!question6option1.isChecked()) {
+
+        setAllOptions = question5option1.isChecked();
+        if (question5option2.isChecked()) {
             setAllOptions = false;
         }
-        CheckBox question6option2 = (CheckBox) findViewById(R.id.q5answer2);
-        if (question6option2.isChecked()) {
+        if (!question5option3.isChecked()) {
             setAllOptions = false;
         }
-        CheckBox question6option3 = (CheckBox) findViewById(R.id.q5answer3);
-        if (!question6option3.isChecked()) {
-            setAllOptions = false;
-        }
-        CheckBox question6option4 = (CheckBox) findViewById(R.id.q5answer4);
-        if (!question6option4.isChecked()) {
+        if (!question5option4.isChecked()) {
             setAllOptions = false;
         }
         if (setAllOptions) {
@@ -86,8 +104,7 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         // Process question no. 6
-        RadioGroup question5RadioGroup = (RadioGroup) findViewById(R.id.q6_radiobuttongroup);
-        if (question5RadioGroup.getCheckedRadioButtonId() == R.id.q6answer1) {
+        if (question6RadioGroup.getCheckedRadioButtonId() == R.id.q6answer1) {
             points = points + 1;
         }
 
@@ -101,6 +118,7 @@ public class QuizActivity extends AppCompatActivity {
         } else {
             nickName = "!";
         }
+
         // Messages for achieved score.
         String scoreMessage = "";
         if (points == 6) {
@@ -125,35 +143,33 @@ public class QuizActivity extends AppCompatActivity {
         Toast.makeText(this, scoreMessage, Toast.LENGTH_LONG).show();
     }
 
+    // Set the correct answers for the quiz
     public void showCorrectAnswers(View view) {
+        // Disable Submit Quiz button
+        submitQuizBtn.setEnabled(false);
 
-        RadioGroup question1RadioGroup = (RadioGroup) findViewById(R.id.q1_radiobuttongroup);
+        //Question 1:
         question1RadioGroup.check(R.id.q1answer4);
 
-        CheckBox question2option1 = (CheckBox) findViewById(R.id.q2answer1);
+        //Question 2:
         question2option1.setChecked(true);
-        CheckBox question2option2 = (CheckBox) findViewById(R.id.q2answer2);
         question2option2.setChecked(false);
-        CheckBox question2option3 = (CheckBox) findViewById(R.id.q2answer3);
         question2option3.setChecked(true);
 
-        EditText question3Text = (EditText) findViewById(R.id.q3answer1);
+        //Question 3:
         question3Text.setText("Java");
 
-        RadioGroup question4RadioGroup = (RadioGroup) findViewById(R.id.q4_radiobuttongroup);
+        //Question 4:
         question4RadioGroup.check(R.id.q4answer2);
 
-        CheckBox question5option1 = (CheckBox) findViewById(R.id.q5answer1);
+        //Question 5:
         question5option1.setChecked(true);
-        CheckBox question5option2 = (CheckBox) findViewById(R.id.q5answer2);
         question5option2.setChecked(false);
-        CheckBox question5option3 = (CheckBox) findViewById(R.id.q5answer3);
         question5option3.setChecked(true);
-        CheckBox question5option4 = (CheckBox) findViewById(R.id.q5answer4);
         question5option4.setChecked(true);
 
-        RadioGroup question6RadioGroup = (RadioGroup) findViewById(R.id.q6_radiobuttongroup);
+        //Question 6:
         question6RadioGroup.check(R.id.q6answer1);
-
     }
 }
+
